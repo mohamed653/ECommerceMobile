d8b4559e8a897f09921f487d79febe01ae0b1b28
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-06-24 12:52:04 +0300

    making new endpoint to add subcategory value with post verb, some progress in reports

---
+++ ECommereceApi/DTOs/Order/OrderPreviewWithoutOffersDTO.cs
@@ -0,0 +1,17 @@
+﻿using ECommereceApi.DTOs.Product;
+
+namespace ECommereceApi.DTOs.Order
+{
+    public class OrderPreviewWithoutOffersDTO
+    {
+        public int UserId { get; set; }
+        public List<ProductDisplayInCartDTO> ProductsAmounts { get; set; } = new();
+        public double FinalPrice { get; set; }
+        public int numberOfUniqueProducts { get; set; } = 0;
+        public int numberOfProducts { get; set; } = 0;
+        public string Governerate { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
