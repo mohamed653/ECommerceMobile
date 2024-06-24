aa2627bd65ca3c0d206c8ad3dfc435e3eeee8616
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-06-24 09:05:38 +0300

    making Mapper map image uri with image id for product

---
+++ ECommereceApi/Services/Mapper/ImageUriCustomResolver.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ECommereceApi.DTOs.Product;
+using ECommereceApi.Services.Interfaces;
+
+namespace ECommereceApi.Services.Mapper
+{
+    public class ImageUriCustomResolver : IValueResolver<ProductImage, ProductImageDTO, string>
+    {
+        private readonly IFileCloudService _fileCloudService;
+        public ImageUriCustomResolver(IFileCloudService fileCloudService)
+        {
+            _fileCloudService = fileCloudService;
+        }
+        public string Resolve(ProductImage source, ProductImageDTO destination, string destMember, ResolutionContext context)
+        {
+                return _fileCloudService.GetImageUrl(source.ImageId);
+        }
+    }
+}
--- ECommereceApi/Services/Mapper/MyMapperConfiguration.cs
+++ ECommereceApi/Services/Mapper/MyMapperConfiguration.cs
@@ -1,27 +1,35 @@
 ﻿using AutoMapper;
 using ECommereceApi.DTOs.Account;
+using ECommereceApi.DTOs.Order;
 using ECommereceApi.DTOs.Product;
 using ECommereceApi.Models;
+using ECommereceApi.Services.classes;
+using ECommereceApi.Services.Interfaces;
 
 namespace ECommereceApi.Services.Mapper
 {
     public class MyMapperConfiguration : Profile
     {
+        //private readonly IFileCloudService _fileCloudServer;
         public MyMapperConfiguration()
         {
+
+            //_fileCloudServer = fileCloudService;
+
             CreateMap<Product, ProductDisplayDTO>()
                 .ForMember(p => p.CategoryName, option => option.MapFrom(p => p.Category.Name))
                 .ReverseMap();
-            
+
             CreateMap<ProductDisplayDTO, ProductDisplayInCartDTO>().ReverseMap();
 
             CreateMap<ProductImage, ProductImageDTO>()
+                .ForMember(p => p.ImageUri, option => option.MapFrom<ImageUriCustomResolver>())
                 .ReverseMap();
 
             CreateMap<Product, ProductAddDTO>().ReverseMap();
 
             CreateMap<Category, CategoryDTO>()
-                .ForMember(c=> c.SubCategories, opt => opt.MapFrom(sc => sc.Subs.Select(s => s.SubCategory)))
+                .ForMember(c => c.SubCategories, opt => opt.MapFrom(sc => sc.Subs.Select(s => s.SubCategory)))
                 .ReverseMap();
 
             CreateMap<SubCategory, SubCategoryDTO>()
@@ -41,12 +49,17 @@ namespace ECommereceApi.Services.Mapper
 
             // Offers
             CreateMap<Offer, OfferDTO>().ReverseMap();
-            CreateMap<OffersDTOUI,Offer > ().ReverseMap();
+            CreateMap<OffersDTOUI, Offer>().ReverseMap();
             CreateMap<ProductOffer, OfferProductsDetailedDTO>().ReverseMap();
             CreateMap<OfferProductsDetailedDTO, ProductOffer>().ReverseMap();
 
             //WebInfo
             CreateMap<ECommereceApi.Models.WebInfo, WebInfoDTO>().ReverseMap();
+
+            #region Orders
+            CreateMap<OrderPreviewDTO, CartProductsDTO>().ReverseMap();
+            CreateMap<OfferDisplayDTO, Offer>().ReverseMap();
+            #endregion
 
         }
     }
