cb8f3b7c4f0b09692920c994f2e8f7db5bcf33fb
Ahmad Esam <90632545+Ahmad-Esam12398@users.noreply.github.com>
2024-06-29 17:11:38 +0300

    Add new 2 endpoints in cart, delete one product and update product quantity

--- ECommereceApi/Controllers/CartController.cs
+++ ECommereceApi/Controllers/CartController.cs
@@ -20,7 +20,7 @@ namespace ECommereceApi.Controllers
         }
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<IActionResult> GetUserCart(int id)
+        public async Task<IActionResult> GetUserCartAsync(int id)
         {
             User user = await _repo.GetUserByIdAsync(id);
             if (user is null) return BadRequest("No user with this Id");
@@ -29,19 +29,29 @@ namespace ECommereceApi.Controllers
         }
         [HttpDelete]
         [Route("{id:int}")]
-        public async Task<IActionResult> DeleteUserCart(int id)
+        public async Task<IActionResult> DeleteUserCartAsync(int id)
         {
             User user = await _repo.GetUserByIdAsync(id);
             if(user is null) return BadRequest("No user with this Id");
             await _repo.DeleteCartItemsAsync(user);
             return Ok();
         }
+        [HttpDelete]
+        [Route("{UserId:int}/{ProductId:int}")]
+        public async Task<IActionResult> DeleteProductFromCartAsync(int UserId, int ProductId)
+        {
+            if(!await _repo.IsUserExistsByIdAsync(UserId)) return NotFound("No User With this Id");
+            if(!await _repo.IsProductExistsByIdAsync(ProductId)) return NotFound("No Product with this Id");
+            if(!await _repo.IsProductExistsInCartAsync(ProductId, UserId)) return NotFound("Product Is not in cart");
+            await _repo.DeleteProductFromCartAsync(ProductId, UserId);
+            return Ok();
+        }
         [HttpPost]
-        public async Task<IActionResult> AddProductToCart(int userId, int productId, int amount)
+        public async Task<IActionResult> AddProductToCartAsync(int userId, int productId, int amount)
         {
             User user = await _repo.GetUserByIdAsync(userId);
             if(user is null) return BadRequest("No user with this Id");
-            var product = await _productRepo.GetProductByIdAsync(productId);
+            var product = await _productRepo.GetProductDisplayDTOByIdAsync(productId);
             if(product is null) return BadRequest("No product with this Id");
             if(amount <= 0) return BadRequest("Invalid Amount Value!");
             if(amount > product.Amount) return BadRequest("There Are no Enough Amount");
@@ -49,7 +59,7 @@ namespace ECommereceApi.Controllers
             return Ok();
         }
         [HttpPut]
-        public async Task<IActionResult> ClearAndAddMultipleProductsToCart(MultipleProductsCardDTO cartProducts)
+        public async Task<IActionResult> ClearAndAddMultipleProductsToCartAsync(MultipleProductsCardDTO cartProducts)
         {
             if(cartProducts.Amounts.Count != cartProducts.ProductsIds.Count || cartProducts.Amounts.Count == 0)
                 return BadRequest("Invalid number of items");
@@ -58,6 +68,17 @@ namespace ECommereceApi.Controllers
             if(!await _productRepo.IsAllProductsExistsAsync(cartProducts.ProductsIds)) return BadRequest("invalid Products Ids");
             await _repo.DeleteCartItemsAsync(user);
             await _repo.AddProductsToCartAsync(user, cartProducts.ProductsIds.ToList(), cartProducts.Amounts);
+            return Ok(await _repo.GetCartProductsAsync(user));
+        }
+        [HttpPatch]
+        public async Task<IActionResult> UpdateProductQuantityAsync(int UserId, int ProductId, int NewQuantity)
+        {
+            if(!await _repo.IsUserExistsByIdAsync(UserId)) return NotFound("No User With this Id");
+            if(!await _repo.IsProductExistsByIdAsync(ProductId)) return NotFound("No Product with this Id");
+            if(!await _repo.IsProductExistsInCartAsync(ProductId, UserId)) return NotFound("Product Is not in cart");
+            if(!await _repo.IsProductQuantityAvailableinStockAsync(ProductId, NewQuantity)) return BadRequest("Required Quantity Not Available in Stock");
+            await _repo.UpdateProductQuantityInCartAsync(UserId, ProductId, NewQuantity);
+            var user = await _repo.GetUserByIdAsync(UserId);
             return Ok(await _repo.GetCartProductsAsync(user));
         }
     }
--- ECommereceApi/Repo/CartRepo.cs
+++ ECommereceApi/Repo/CartRepo.cs
@@ -25,9 +25,9 @@ namespace ECommereceApi.Repo
             return await _db.Users.Include(u => u.ProductCarts).FirstOrDefaultAsync(u => u.UserId == userId);
         }
 
-        public async Task<CartProductsDTO?> GetCartProductsAsync(User user)
+        public async Task<CartProductsDTO> GetCartProductsAsync(User user)
         {
-            return await MapProductsInCartIntoCartProductsDTO(user.ProductCarts, new CartProductsDTO());
+            return await MapProductsInCartIntoCartProductsDTO(user.ProductCarts, new CartProductsDTO() { UserId = user.UserId });
         }
 
         public async Task DeleteCartItemsAsync(User user)
@@ -50,11 +50,22 @@ namespace ECommereceApi.Repo
             });
             await MySaveChangesAsync();
         }
+        public async Task UpdateProductQuantityInCartAsync(int userId, int productId, int newQuantity)
+        {
+            var target = await _db.ProductCarts.FirstOrDefaultAsync(pc => pc.ProductId == productId && pc.UserId == userId);
+            target.ProductAmount = newQuantity;
+            _db.ProductCarts.Update(target);
+            await MySaveChangesAsync();
+        }
+        public async Task DeleteProductFromCartAsync(int userId, int productId)
+        {
+            _db.Remove<ProductCart>(await _db.ProductCarts.FirstOrDefaultAsync(pc => pc.UserId == userId && pc.ProductId == productId));
+            await MySaveChangesAsync();
+        }
         public async Task MySaveChangesAsync()
         {
             await _db.SaveChangesAsync();
         }
-
         public bool IsUserHaveProductsInCart(User user)
         {
             return user.ProductCarts.Count != 0;
@@ -74,7 +85,7 @@ namespace ECommereceApi.Repo
         {
             foreach (var item in products)
             {
-                ProductDisplayInCartDTO productDisplayDTO = _mapper.Map<ProductDisplayInCartDTO>(await _productRepo.GetProductByIdAsync(item.ProductId));
+                ProductDisplayInCartDTO productDisplayDTO = _mapper.Map<ProductDisplayInCartDTO>(await _productRepo.GetProductDisplayDTOByIdAsync(item.ProductId));
                 productDisplayDTO.Amount = item.ProductAmount;
                 productDisplayDTO.AllAmount = _db.Products.FirstOrDefaultAsync(p => p.ProductId == item.ProductId).Result.Amount;
                 cartProducts.ProductsAmounts.Add(productDisplayDTO);
@@ -83,7 +94,22 @@ namespace ECommereceApi.Repo
             }
             cartProducts.FinalPrice = cartProducts.ProductsAmounts.Sum(pa => pa.FinalPrice.Value * pa.Amount);
             return cartProducts;
-
+        }
+        public async Task<bool> IsUserExistsByIdAsync(int userId)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.UserId == userId) is not null;
+        }
+        public async Task<bool> IsProductExistsByIdAsync(int ProductId)
+        {
+            return await _db.Products.FirstOrDefaultAsync(p => p.ProductId == ProductId) is not null;
+        }
+        public async Task<bool> IsProductExistsInCartAsync(int productId, int userId)
+        {
+            return await _db.ProductCarts.FirstOrDefaultAsync(pc => pc.ProductId == productId && pc.UserId == userId) is not null;
+        }
+        public async Task<bool> IsProductQuantityAvailableinStockAsync(int productId, int quantity)
+        {
+            return (await _db.Products.FirstOrDefaultAsync(p => p.ProductId == productId)).Amount >= quantity;
         }
     }
 }
